b2dbec44e04ed99659ae62e91273963dc94b245c
Albert <albertmoczadlo@gmail.com>
2024-01-14 14:59:11 +0100

    Added middleware

--- NexaCoreERP.Domain/Entities/Product.cs
+++ NexaCoreERP.Domain/Entities/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NexaCoreERP.Domain.Entities
 {
-    internal class Product
+    public class Product
     {
     }
 }
---
+++ NexaCoreERP.Shared/Common/ResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexaCoreERP.Shared.Common
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public string Errors { get; set; }
+    }
+}
